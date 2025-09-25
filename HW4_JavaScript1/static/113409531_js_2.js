eee08f57aa1ad28052f969d28c25e490a7702fd6
chidi0716 <chidi0716@gmail.com>
2025-09-25 15:40:06 +0800

    計算機按鈕輸入與功能按鈕新增

--- HW4_JavaScript1/static/113409531_js_2.js
+++ HW4_JavaScript1/static/113409531_js_2.js
@@ -1,5 +1,24 @@
-document.write('<h2>最新型橫式計算機</h2>');
-document.write('<input id="typeBar" type="text"><br>');
+document.write(`
+  <h2>最新型橫式計算機</h2>
+  <input id="typeBar" type="text"><br>
+`);
+
 for (let i = 0; i <= 9; i++) {
     document.write('<button onclick="add(' + i + ')">' + i + '</button>');
+}
+
+document.write(`
+  <br>
+  <button onclick="add('+')">+</button>
+  <button onclick="add('-')">-</button>
+  <button onclick="add('*')">*</button>
+  <button onclick="add('/')">/</button>
+  <br>
+  <button onclick="calc()">=</button>
+  <button onclick="clearBar()">clear</button>
+`);
+
+function add(input) {
+    const bar = document.getElementById('typeBar');
+    bar.value += input;
 }

4755257912200c47424f80ab7bc1e325919458d7
chidi0716 <chidi0716@gmail.com>
2025-10-02 14:07:48 +0800

    計時停止功能

--- HW5_JavaScript2/static/113409531_js_1.js
+++ HW5_JavaScript2/static/113409531_js_1.js
@@ -24,5 +24,8 @@ function Guess() {
         hint.innerText = `答對了！你總共猜了 ${tries} 次`;
         answer = Math.floor(Math.random() * 101); // 換新題目
         tries = 0;
+        clearInterval(timer); // 停止計時
+        timer = null;         // 清空，讓下一局能重新開始
+        seconds = 0;          // 歸零
     }
 }

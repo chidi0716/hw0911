85546334e86d51ee9dc03563e3939e737fdc7222
chidi0716 <chidi0716@gmail.com>
2025-10-02 13:48:42 +0800

    提示顯示位置更改

--- HW5_JavaScript2/static/113409531_js_1.js
+++ HW5_JavaScript2/static/113409531_js_1.js
@@ -2,17 +2,18 @@ let answer = Math.floor(Math.random() * 101);
 let tries = 0;
 
 const input = document.getElementById('guess');
+const hint = document.getElementById('hint');
 
 function Guess() {
     const value = Number(input.value);
     tries++;
 
     if (value > answer) {
-        alert("太大了")
+        hint.innerText = "太大了！";
     } else if (value < answer) {
-        alert("太小了")
+        hint.innerText = "太小了！";
     } else {
-        alert(`答對了 你總共猜了 ${tries} 次`);
+        hint.innerText = `答對了！你總共猜了 ${tries} 次`;
         answer = Math.floor(Math.random() * 101); // 換新題目
         tries = 0;
     }

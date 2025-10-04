a3ec31fac369e16822e83e2d71931c749ba562a5
chidi0716 <chidi0716@gmail.com>
2025-10-05 04:01:40 +0800

    勾選與金額計算功能

--- HW5_JavaScript2/static/113409531_js_2.js
+++ HW5_JavaScript2/static/113409531_js_2.js
@@ -5,3 +5,24 @@ checkAll.addEventListener('change', () => {
     checkItems.forEach(item => item.checked = checkAll.checked);
     recalc();
 });
+checkItems.forEach(item => {
+    item.addEventListener('change', () => {
+        if (![...checkItems].every(i => i.checked)) {
+            checkAll.checked = false;
+        } else {
+            checkAll.checked = true;
+        }
+        recalc();
+    });
+});
+function recalc() {
+    let sum = 0;
+    checkItems.forEach(item => {
+        if (item.checked) {
+            const row = item.closest('tr');
+            const subtotal = Number(row.querySelector('.subtotal').textContent);
+            sum += subtotal;
+        }
+    });
+    total.textContent = sum;
+}
